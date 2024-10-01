46a1d6407a631104d68fe94bb6b42b37dc70e69e
Monika <monika.cilinska@gmail.com>
2024-10-01 13:31:42 +0200

    restructure

--- src/components/dummy/Button.jsx
+++
@@ -1,6 +0,0 @@
-export default function Button ({text, theme="primary", onClick}) {
-	return (
-		<button className={`btn ${theme} `} onClick={onClick}>{text}</button>
-	);
-}
-  
--- src/components/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer () {
-    return (
-    <div className="w-full h-footer-min px-6 mx-auto lg:mx-0 bg-gray-800 flex justify-center items-center shadow-xl absolute inset-x-0 bottom-0">
-      <div className="text-white leading-7">
-        stopka
-      </div>
-    </div>
-    );
-  }
